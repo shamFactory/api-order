712483211d5485bd5773ccd6cc13c545ed694ae4
Michael Hernandez <michael.hernandez.4790@gmail.com>
2020-03-22 08:10:52 -0500

    add mongodb && fix config

--- src/app.js
+++ src/app.js
@@ -1,14 +1,30 @@
 import express from 'express';
 import httpi from 'http';
-import global from './config/global';
+import globalConfig from './config/globalConfig';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
 const app = express();
 const http = httpi.Server(app);
+
+
+mongoose.Promise = global.Promise;
+mongoose.connect(globalConfig.DB, { useNewUrlParser: true }).then(
+  () => {console.log('Database is connected') },
+  err => { console.log('Can not connect to the database'+ err)}
+);
+mongoose.set('useFindAndModify', false);
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get("/", (req, res) => {
     res.send("Hello from Node.js app \n");
 });
 
-const PORT = global.port || 4000;
+const PORT = globalConfig.port || 4000;
 http.listen(PORT, () => {
     console.log(`Listening on ${PORT}`);
 });
